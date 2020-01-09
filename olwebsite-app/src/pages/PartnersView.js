c4478134c6435e7202b1b907d7287cf004f44df6
Fabricevladimir <Fabricevladimir@gmail.com>
2020-01-08 19:40:50 -0500

    Added SearchBox to PartnersView

--- olwebsite-app/src/pages/PartnersView.js
+++ olwebsite-app/src/pages/PartnersView.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import SearchBox from "../components/SearchBox/SearchBox";
+
+const handleSearch = searchQuery => {
+  // TODO - the searching function should beware of empty strings
+  alert(searchQuery);
+};
+
 const PostsView = () => {
   return (
     <div className="partners-view-container">
       <h1>Partners View</h1>
+      <SearchBox onSearch={handleSearch} />
     </div>
   );
 };
--- olwebsite-app/src/pages/PostsView.js
+++ olwebsite-app/src/pages/PostsView.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import SearchBox from "../components/SearchBox/SearchBox";
 
 const handleSearch = searchQuery => {
