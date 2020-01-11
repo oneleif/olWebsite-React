29352c6db79c25c22aee2497ea54769caf4878ee
Will <will.lambert96@gmail.com>
2020-01-11 15:08:08 -0500

    Create Card Container

---
+++ olwebsite-app/src/pages/PostsView.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import CardContainer from "../components/Objects/CardContainer/CardContainer";
+
+export default function PostsView() {
+  /************************************
+   * Render
+   ************************************/
+
+  return (
+    <div className="posts-view-container">
+      <h1>Posts View</h1>
+      <CardContainer>
+        //TODO: Pass Children/Cards in here
+      </CardContainer>
+    </div>
+  );
+};
