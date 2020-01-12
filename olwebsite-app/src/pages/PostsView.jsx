7272319bb8059933575a2b7029f240b77221627b
Will <will.lambert96@gmail.com>
2020-01-12 17:43:35 -0500

    Create base style card

--- olwebsite-app/src/pages/PostsView.jsx
+++ olwebsite-app/src/pages/PostsView.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 
 import CardContainer from "../components/Objects/CardContainer/CardContainer";
+import Card from "../components/Objects/Card/Card";
+import CardHeader from "../components/Objects/Card/CardHeader";
+import CardContent from "../components/Objects/Card/CardContent";
+import CardFooter from "../components/Objects/Card/CardFooter";
 
 export default function PostsView() {
   /************************************
    * Render
    ************************************/
 
+   const dummyCards = [
+       {
+           header: "Test Header"
+       },
+       {
+           header: "Test Header"
+       },
+       {
+           header: "Test Header"
+       },
+       {
+           header: "Test Header"
+       },
+       {
+           header: "Test Header"
+       }
+    ];
   return (
     <div>
       <h1>Posts View</h1>
       <CardContainer>
-        {/* //TODO: Pass Children/Cards in here */}
+      {dummyCards.map((card, index) => (
+        <Card key={index}>
+            <CardHeader text={card.header}/>
+            <CardContent>
+                <br/>
+            </CardContent>
+            <CardFooter>
+                <br/>
+            </CardFooter>
+        </Card>
+      ))}
       </CardContainer>
     </div>
   );
