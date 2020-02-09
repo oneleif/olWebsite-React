48dfa67a858d6fd8c1eba2559a53a0ba8e68613a
Will <will.lambert96@gmail.com>
2020-02-09 18:35:03 -0500

    quotes

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import  React, { useContext, useEffect, useState } from "react";
+import  React, { useContext, useEffect, useState } from 'react';
 
 const UserContext = React.createContext([undefined, undefined]);
 
