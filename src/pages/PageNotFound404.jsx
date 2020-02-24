fabd4828c1c587b3bedb2a5802b2addb73b7263b
Daniel Lin <daniel@aikon.com>
2020-02-24 09:53:40 -0800

    Render location pathname

--- src/pages/PageNotFound404.jsx
+++ src/pages/PageNotFound404.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function PageNotFound404() {
   /************************************
    * Render
    ************************************/
+  let location = useLocation();
 
   return (
     <div className="container-404">
       <div className="body-404">
         <h1>404</h1>
+        <code>{location.pathname}</code>
         <h3>Page Not Found</h3>
         <p>Sorry, the page you are looking for doesn't seem to exist!</p>
         <Link to="/">Go to Home</Link>
