0fdd9142d3eea4202cf7ccedfb21561d4036f46a
Jacob Jiskoot <jacobjiskoot@Jacobs-MacBook-Pro.local>
2020-02-23 22:08:53 -0500

    Use ProtecteRoute for profile page

--- src/OlWebsiteApp.jsx
+++ src/OlWebsiteApp.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProtectedRoute from './components/Objects/ProtectedRoute/ProtectedRoute';
 
 import "./style-sheets/main.scss";
 
 import ToolbarContainer from "./components/Toolbar/ToolbarContainer";
 import Footer from "./components/Footer";
 import LandingView from "./pages/LandingView";
-import PostsView from "./pages/PostsView";
-import RegisterView from "./pages/RegisterView";
 import LoginView from "./pages/LoginView";
+import PostsView from "./pages/PostsView";
+import ProfileView from "./pages/ProfileView";
+import RegisterView from "./pages/RegisterView";
 import { UserProvider } from "./contexts/UserContext";
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -23,16 +25,17 @@ export default function OlWebsiteApp() {
       <div className="app">
         <Router basename={process.env.PUBLIC_URL}>
           <ToolbarContainer />
-            <ScrollToTop>
-              <div className="app-body">
-                <Switch>
-                  <Route path="/login" component={LoginView} />
-                  <Route path="/posts" component={PostsView} />
-                  <Route path="/sign-up" component={RegisterView} />
-                  <Route path="/" component={LandingView} />
-                </Switch>
-              </div>
-            </ScrollToTop>
+          <ScrollToTop>
+            <div className="app-body">
+              <Switch>
+                <Route path="/login" component={LoginView} />
+                <Route path="/posts" component={PostsView} />
+                <ProtectedRoute path="/profile" component={ProfileView} />
+                <Route path="/sign-up" component={RegisterView} />
+                <Route path="/" component={LandingView} />
+              </Switch>
+            </div>
+          </ScrollToTop>
           <Footer />
         </Router>
       </div>
--- src/pages/ProfileView.jsx
+++ src/pages/ProfileView.jsx
@@ -29,35 +29,33 @@ export default function ProfileView() {
 
     return (
         <div className="profile-view-body">
-            {user && <>
-                <div className="user-info">
-                    {user[SOCIAL][PROFILE_IMAGE] ?
-                        <img className="user-icon" src={user[SOCIAL][PROFILE_IMAGE]} alt="user icon" />
-                        : <img className="user-icon" src={IconPlaceholder} alt="user icon placeholder" />}
-                    <div className="user-full-name">{user[SOCIAL][FIRST_NAME] || "Auser"} {user[SOCIAL][LAST_NAME] || "Name"}</div>
-                    <div className="username">@{user[SOCIAL][USERNAME] || "ausername"}</div>
-                    <div className="user-location"><FaMapMarkerAlt /> {user[SOCIAL][LOCATION] || "Aplace, AbiggerPlace"}</div>
-                    <div className="user-social-info">
-                        <div><FaEnvelope /> {user[EMAIL] || "anaccount@ado.main"}</div>
-                        <div><FaDiscord /> {user[SOCIAL][DISCORED_USERNAME] || "AdiscordUserName"}</div>
-                        <div><FaGithubSquare /> {user[SOCIAL][GITHUB_USERNAME] || "GotAccount?"}</div>
-                        {/* TODO: Add linkedIn account value, after added on backend model */}
-                        <div><FaLinkedin /> Replace LinkedIn value</div>
-                        {user[SOCIAL][LINKS] && user[SOCIAL][LINKS].map(link =>
-                            <div key={link}><FaLink /> <a href={link}> {link}</a></div>
-                        )}
+            <div className="user-info">
+                {user[SOCIAL][PROFILE_IMAGE] ?
+                    <img className="user-icon" src={user[SOCIAL][PROFILE_IMAGE]} alt="user icon" />
+                    : <img className="user-icon" src={IconPlaceholder} alt="user icon placeholder" />}
+                <div className="user-full-name">{user[SOCIAL][FIRST_NAME] || "Auser"} {user[SOCIAL][LAST_NAME] || "Name"}</div>
+                <div className="username">@{user[SOCIAL][USERNAME] || "ausername"}</div>
+                <div className="user-location"><FaMapMarkerAlt /> {user[SOCIAL][LOCATION] || "Aplace, AbiggerPlace"}</div>
+                <div className="user-social-info">
+                    <div><FaEnvelope /> {user[EMAIL] || "anaccount@ado.main"}</div>
+                    <div><FaDiscord /> {user[SOCIAL][DISCORED_USERNAME] || "AdiscordUserName"}</div>
+                    <div><FaGithubSquare /> {user[SOCIAL][GITHUB_USERNAME] || "GotAccount?"}</div>
+                    {/* TODO: Add linkedIn account value, after added on backend model */}
+                    <div><FaLinkedin /> Replace LinkedIn value</div>
+                    {user[SOCIAL][LINKS] && user[SOCIAL][LINKS].map(link =>
+                        <div key={link}><FaLink /> <a href={link}> {link}</a></div>
+                    )}
 
-                    </div>
-                    <div className="user-social-info">
-                        <div className="user-bio-header">User Bio</div>
-                        <div>{user[SOCIAL][BIOGRAPHY] || "Bio me up"}</div>
-                    </div>
-                    <button type="button">
-                        <FaPencilAlt /> Edit Your Info
-                    </button>
                 </div>
-                <div className="user-tabs"></div>
-            </>}
+                <div className="user-social-info">
+                    <div className="user-bio-header">User Bio</div>
+                    <div>{user[SOCIAL][BIOGRAPHY] || "Bio me up"}</div>
+                </div>
+                <button type="button">
+                    <FaPencilAlt /> Edit Your Info
+                    </button>
+            </div>
+            <div className="user-tabs"></div>
         </div>
     );
 };
