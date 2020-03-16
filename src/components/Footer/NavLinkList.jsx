87005eaf821a19452b76b0f3512157d33ca9ecbc
Will <will.lambert96@gmail.com>
2020-03-16 18:06:00 -0400

    add docs for NavLinkList Component

--- src/components/Footer/NavLinkList.jsx
+++ src/components/Footer/NavLinkList.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
+
+/**
+* Component that Displays a header with an unordered list of links.
+* Takes in a {@code String} header and an array of objects that contain a label and path
+*
+*  For example: [{ link: 'link', path: '/path'}]
+*/
 function NavLinkList({ header, links }) {
   /************************************
    * Render
