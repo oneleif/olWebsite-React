e135c3ba38a283c07a8687b0fb40f6458b0e9e2f
Will <will.lambert96@gmail.com>
2020-03-16 18:42:51 -0400

    Make duplicate code into a modular component, test for that and add docs

--- src/components/FeatureContainer/FeatureContainer.jsx
+++ src/components/FeatureContainer/FeatureContainer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+/**
+* Component that displays a container with text children passed in aligned opposite from an image
+* Takes in a {@class JSX} image and a (@class boolean) invert, that indicates with side to position the text/
+* Invert being false has the text on the left, true it is positioned on the
+* 
+*/
 export default function FeatureContainer({ children, image, invert }) {
   /************************************
    * Render
