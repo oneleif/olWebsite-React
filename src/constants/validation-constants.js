78798076de7bcd8ae8bc21ac106428ba73ecc2f9
Fabricevladimir <fabricevladimir@gmail.com>
2020-04-08 23:16:55 -0400

    Formatting fixes

--- src/constants/validation-constants.js
+++ src/constants/validation-constants.js
@@ -17,15 +17,13 @@ export const REGISTER_CONSTANTS = {
   PASSWORD_PROPERTY,
   REQUIRED_PASSWORD_LENGTH,
   CONFIRMED_PASSWORD_PROPERTY,
-
   EMAIL_SCHEMA: new Schema().isEmail().isRequired(),
-
+  CONFIRMED_PASSWORD_SCHEMA: new Schema().isRequired().matches(PASSWORD_PROPERTY),
   PASSWORD_SCHEMA: new Schema()
     .hasDigit()
     .hasSymbol()
     .isRequired()
     .hasLowercase()
     .hasUppercase()
-    .min(REQUIRED_PASSWORD_LENGTH),
-  CONFIRMED_PASSWORD_SCHEMA: new Schema().isRequired().matches(PASSWORD_PROPERTY)
+    .min(REQUIRED_PASSWORD_LENGTH)
 };
