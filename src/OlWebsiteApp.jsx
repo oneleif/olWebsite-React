f40cc666ff64d3da0d7a23daef3a051a0e5c1664
basil <basel_marwan@icloud.com>
2020-04-20 09:28:03 +0200

    Basic Active Projects View

--- src/OlWebsiteApp.jsx
+++ src/OlWebsiteApp.jsx
@@ -9,6 +9,7 @@ import PageNotFoundView from './pages/PageNotFoundView';
 import Toolbar from './components/Toolbar/Toolbar';
 import { UserProvider } from './contexts/UserContext';
 import './style-sheets/main.scss';
+import ActiveProjectsView from './pages/ActiveProjectsView';
 
 export default function OlWebsiteApp() {
   /************************************
@@ -25,6 +26,7 @@ export default function OlWebsiteApp() {
               <Switch>
                 <Route exact path='/contact' component={ContactUsView} />
                 <Route exact path='/' component={LandingView} />
+                <Route exact path= '/projects' component={ActiveProjectsView}/>
                 <Route component={PageNotFoundView} />
               </Switch>
             </div>
--- src/pages/ActiveProjectsView.jsx
+++ src/pages/ActiveProjectsView.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
+import FeatureContainer from '../components/FeatureContainer/FeatureContainer';
+import FeatureCopy from '../components/FeatureContainer/FeatureCopy';
+import Button from '../components/Objects/Button/Button';
+import SocialMediaConstants from '../constants/social-media-constants';
+import ActiveProjectsSVG from '../assets/ActiveProjectsSVG/ActiveProjectsSVG';
+
+
 export default function ActiveProjectsView() {
-  return <h1>Active Projects</h1>;
+  /************************************
+   * Render
+   ************************************/
+
+  return (
+    <div>
+      <FeatureContainer image={<ActiveProjectsSVG/>}>
+        <FeatureCopy header='Active Projects'>
+          Find a project that interests you, and join the team to start learning. Don’t see an interesting project? We will help you start one!
+        </FeatureCopy>
+        <Button handleClick = {() => window.open(SocialMediaConstants.DISCORD_LINK,"_blank")}>Join our Discord!</Button>
+      </FeatureContainer>
+    </div>
+  )
 }
