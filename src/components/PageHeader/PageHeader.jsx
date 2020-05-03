26f8fe664aff57c6e3072b9307ab16ca6d7fc672
Will <will.lambert96@gmail.com>
2020-05-03 00:23:09 -0400

    Add docs

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import Button from '../Objects/Button/Button';
 import SocialMediaConstants from "../../constants/social-media-constants";
 
+/**
+ * This is a Header component to be used by large page views (see Projects/Meet the team) 
+ * This Component shows a title, image, children (prefer this to be <p>copy</p>) and a button
+ * @param {String} title - title of the page
+ * @param {JSX} img - image component
+ * @param {JSX} children - copy wrapped in <p>
+ */
 export default function PageHeader({ title, img, children }) {
   /************************************
    * Render
