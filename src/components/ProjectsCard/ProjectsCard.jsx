96d547da3fa6538c0a64267dd3ffedd0e7243a10
Will <will.lambert96@gmail.com>
2020-07-11 14:23:05 -0400

    Formatting

--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -8,25 +8,25 @@ import CardTagContainer from '../Objects/Card/CardTagContainer';
 
 export default function ProjectsCard({ project }) {
 	/************************************
-	 * Render
-	 ************************************/
+   * Render
+   ************************************/
 
-	return (
+  return (
     <Card className='projects-card'>
-			<CardContent>
-				<img src={project?.img ? project.img : DefaultProjectImg} alt={project.name}></img>
-				<h4>{project.name}</h4>
-				<h3>{project.type}</h3>
-				<p>{project.details}</p>
-			</CardContent>
-    <CardFooter>
-			<CardTagContainer>
-				{/* // TODO: calculate tags to show based off the width of the view */}
-				{project.tags.map((tag, index) => (
-					<Tag key={index}>{tag}</Tag>
-				))}
-			</CardTagContainer>
-    </CardFooter>
-    </Card>
+		  <CardContent>
+			  <img src={project?.img ? project.img : DefaultProjectImg} alt={project.name}></img>
+			  <h4>{project.name}</h4>
+			  <h3>{project.type}</h3>
+			  <p>{project.details}</p>
+		  </CardContent>
+		  <CardFooter>
+			  <CardTagContainer>
+				  {/* // TODO: calculate tags to show based off the width of the view */}
+				  {project.tags.map((tag, index) => (
+					  <Tag key={index}>{tag}</Tag>
+				  ))}
+			  </CardTagContainer>
+		  </CardFooter>
+		</Card>
 	);
 }
