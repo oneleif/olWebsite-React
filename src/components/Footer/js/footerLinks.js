f5453cdc00e671d30192f8f243d4313bc10d8628
roramigator <roberto@morado.dev>
2021-01-22 20:10:49 -0500

    projects up to date and rendering

--- src/components/Footer/js/footerLinks.js
+++ src/components/Footer/js/footerLinks.js
@@ -1,6 +1,62 @@
+import projectCards from '../../../pages/js/projects.js';
+
+//  static data can be generated from the projects.js file
+// [
+//   {
+//     label: 'Oneleif',
+//     path: '/projects',
+//     internal: true
+//   },
+//   {
+//     label: 'Bread for the Journey',
+//     path: '/projects',
+//     internal: true
+//   },
+//   {
+//     label: 'Swift UI Kit',
+//     path: '/projects',
+//     internal: true
+//   },
+//   {
+//     label: 'Leifdown',
+//     path: '/projects',
+//     internal: true
+//   },
+//   {
+//     label: 'Filodex',
+//     path: '/projects',
+//     internal: true
+//   },
+//   {
+//     label: 'Onefit',
+//     path: '/projects',
+//     internal: true
+//   }
+// ]
+
+/************************************
+ * Proposal
+ ************************************/
+// the data can be generated creating the array of objects
+// pros: less code
+// cons: logic implementation if path and internal differ between objects
+
+const parseLinks = projectCards.map(project => {
+  return {
+    label: project.name,
+    path: '/projects',
+    internal: true
+  };
+});
+
+/************************************
+ *
+ ************************************/
+
 const footerLinks = [
   {
-    header: 'Projects'
+    header: 'Projects',
+    links: parseLinks
   },
   {
     header: 'Resources',
